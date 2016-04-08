f65223d9b4030f80fcd97ba4ace71c4496b644f9
Juan G. Jordán <juangui@gmail.com>
2016-04-08 03:52:19 +0200

    Completed sample app

--- sample/js/app.js
+++ sample/js/app.js
@@ -23,19 +23,45 @@ angular.module('starter', ['ionic', 'ngFlic'])
   });
 })
 
-.controller('MyCtrl', function($scope, $ionicPlatform, $timeout, $cordovaFlic) {
-  var appId = "dbcce1f9-c4b9-41c1-89fb-2f36c8577706";
-  var appSecret = "f17c4448-093b-4ba8-951a-bb40113b1900";
-  var appName = "Table Football";
-
-  $scope.status = 'Initiating flic...';
+.controller('MyCtrl', function($scope, $rootScope, $ionicPlatform, $timeout, $cordovaFlic) {
 
   $ionicPlatform.ready(function() {
 
+    var appId = "dbcce1f9-c4b9-41c1-89fb-2f36c8577706";
+    var appSecret = "f17c4448-093b-4ba8-951a-bb40113b1900";
+    var appName = "Table Football";
+
+    $scope.status = 'Initiating flic...';
+    $scope.buttons = [];
+
+    $scope.grabButton = function() {
+      $scope.status = 'Grabbing button...';
+      $cordovaFlic.grabButton().then(function(button) {
+        $scope.status = 'Grabbed a button.';
+        $scope.buttons.push(button);
+      }, function(error) {
+        $scope.status = 'GrabButton error: ' + error;
+      });
+    }
+
     $cordovaFlic.init(appId, appSecret, appName).then(function(result) {
-      $scope.status = 'Flic init success';
+      $scope.status = 'Flic init success.\nGetting known buttons...';
+
+        $cordovaFlic.getKnownButtons().then(function(buttons) {
+          $scope.status = 'Got ' + (buttons.length > 0 ? buttons.length : 'no') + ' buttons.';
+          buttons.forEach(function(button) {
+            $scope.buttons.push(button);
+          });
+        }, function(error) {
+          $scope.status = 'GetKnownButtons error: ' + error;
+        });
+
     }, function(error) {
       $scope.status = 'Flic init error: ' + error;
+    });
+
+    $rootScope.$on('$cordovaFlic:flicButtonPressed', function (event, data) {
+      $scope.status = data.button.color + ' button received ' + data.event + ' event.';
     });
 
   }, false);
